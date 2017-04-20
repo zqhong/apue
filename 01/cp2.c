7435816405e9ba9942836502f09cb86d6a73046f
zqhong <zqh0513@gmail.com>
2017-04-20 23:56:26 +0800

    added: complete cp2 example and improve README.md

---
+++ 01/cp2.c
@@ -0,0 +1,18 @@
+#include "../include/apue.h"
+#include "../lib/error.c"
+
+int main(void) {
+    int c;
+
+    while ((c = getc(stdin)) != EOF) {
+        if (putc(c, stdout) == EOF) {
+            err_sys("output error");
+        }
+    }
+
+    if (ferror(stdin)) {
+        err_sys("input error");
+    }
+
+    exit(0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# TODO
+## 使用
+### 编译
+```
+$ make
+```
+
+### 删除所有二进制文件
+```
+$ make clean
+```
+
+### 使用某个二进制文件
+```
+// 以 01 目录下的 ls 为例，请确保你运行了 make 命令完成编译
+$ 01/ls .
+  ..
+  .gitignore
+  lib
+  01
+  .git
+  LICENSE
+  include
+  README.md
+  .
+  Makefile
+  .idea
+```
