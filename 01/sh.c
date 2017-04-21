d588cf0b42a3a26f32b49c5b632d146e673aa565
zqhong <zqh0513@gmail.com>
2017-04-21 23:56:26 +0800

    improved: using cmake as build tool && add sh.c example

---
+++ 01/sh.c
@@ -0,0 +1,33 @@
+#include "../include/apue.h"
+#include "../lib/error.c"
+#include "/usr/include/sys/wait.h"
+
+int main(void) {
+    char buf[MAXLINE];
+    pid_t pid;
+    int status;
+
+    // print prompt
+    printf("%% ");
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        if (buf[strlen(buf) - 1] == '\n') {
+            buf[strlen(buf) - 1] = 0;
+        }
+
+        if ((pid = fork()) < 0) {
+            err_sys("fork error");
+        } else if (pid == 0) {
+            // child
+            execlp(buf, buf, (char *) 0);
+            err_ret("couldn't execute: %s", buf);
+        }
+
+        // parent
+        if ((pid = waitpid(pid, &status, 0)) < 0) {
+            err_sys("waitpid error");
+        }
+        printf("%% ");
+    }
+
+    exit(0);
+}
