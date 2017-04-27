389494b8376691695899f3eb3ea217b4682face7
zqhong <zqh0513@gmail.com>
2017-04-27 22:40:28 +0800

    added: add fileflags, setflsg examples

---
+++ src/03_fileio/setflag.c
@@ -0,0 +1,19 @@
+#include <apue.h>
+#include <fcntl.h>
+#include <error.c>
+
+void set_fl(int fd, int flags) {
+    // flags are file status flags to turn on
+    int val;
+
+    if ((val = fcntl(fd, F_GETFL, 0)) < 0) {
+        err_sys("fcntl F_GETFL error");
+    }
+
+    // turn on flags
+    val |= flags;
+
+    if (fcntl(fd, F_SETFL, val) < 0) {
+        err_sys("fcntl F_SETFL error");
+    }
+}
